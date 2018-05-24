995b0ded83ebd1b5ecafc0c6a5f87b68ebc203ba
miyaluc <srlucas3@gmail.com>
2018-05-24 14:05:44 -0700

    middle element solution still needs work

---
+++ InterviewPrep/InterviewPrep/ReturnMiddleElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPrep
+{
+    class ReturnMiddleElement
+    {
+        //return the middle element of a linked list
+
+        /// <summary>
+        /// Traverse linked list using two pointers. Move one pointer by one and other pointer by two. When the fast pointer reaches the end, the slow pointer will reach middle of linked list.
+        /// </summary>
+        public void PrintMiddle()
+        {
+            LinkedList<int> ll = new LinkedList<int>();
+            
+            Node head;
+
+            Node walker = head;
+            Node runner = head;
+
+        }
+    }
+}
